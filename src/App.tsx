fec51d47d2015d6f60d04b667c0fca3274bc1d22
Cu-Toof <Cu-Toof@users.noreply.github.com>
2024-06-08 10:41:30 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { TonConnectButton } from '@tonconnect/ui-react'
+import '@twa-dev/sdk'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <TonConnectButton/>
+    </div>
+  )
+}
+
+export default App
